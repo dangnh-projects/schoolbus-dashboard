32c6cc762859ecde080819411650e2e50706aaf2
Dang Nguyen <dangnh@nhg.vn>
2019-11-21 10:42:30 +0700

    integrate parent

--- src/views/Dashboard/Parent/Form/index.js
+++ src/views/Dashboard/Parent/Form/index.js
@@ -11,164 +11,225 @@ import {
   DatePicker,
   Upload,
   Icon,
+  message,
+  Button,
 } from 'antd';
 import moment from 'moment';
 import { connect } from 'react-redux';
 import { navigate } from '@reach/router';
-import BaseForm from 'components/Form';
 import { actionCreator } from 'store/dataTable/dataTable.meta';
-import { API } from 'api/metaData';
+//import { API } from 'api/metaData';
 
 const Item = Form.Item;
 
 const ParentForm = ({ formSave, updateItem, id, data }) => {
   const [item, setItem] = useState(null);
-  const [courses, setCourses] = useState([]);
 
-  const getCourses = async () => {
-    const { body } = await API.getCourse();
-    setCourses(body.results);
-  };
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [birthday, setBirthday] = useState();
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [idPassport, setIdPassport] = useState('');
+  //const [chilren, setChildren] = useState('');
 
-  const handleSubmit = fields => {
-    if (fields.start_time > fields.end_time) {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  const [homeNumber, setHomeNumber] = useState();
+  const [homeWard, setHomeWard] = useState();
+  const [homeDistrict, setHomeDistrict] = useState();
+  const [homeProvince, setHomeProvince] = useState();
+
+  const [avatar, setAvatar] = useState();
+  const [imgVal, setImgVal] = useState();
+
+  const handleSubmit = () => {
+    const fields = {
+      first_name: firstName,
+      last_name: lastName,
+      birthday,
+      phone_number: phoneNumber,
+      id_number: idPassport,
+      //chilren,
+      //homeNumber,
+      //homeWard,
+      //homeDistrict,
+      //homeProvince,
+      username,
+      password,
+    };
+    if (avatar) {
+      fields.avatar = avatar;
+    }
+    if (birthday > moment()) {
       notification.error({
-        message: 'End time must not be set before start time',
+        message: 'Birthday must not be after current date',
       });
       return;
     }
+    fields.birthday = fields.birthday.format('YYYY-MM-DD');
 
-    fields.start_time = fields.start_time.format('YYYY-MM-DD');
-    fields.end_time = fields.end_time.format('YYYY-MM-DD');
     if (id) {
       fields.id = id;
+      console.log('id: ' + fields.info);
       updateItem({
-        url: '/r/batches/',
+        url: '/core/api/parent/',
         data: fields,
-        afterSave: () => navigate('/dashboard/batch'),
+        afterSave: () => navigate('/dashboard/parent'),
       });
     } else {
       formSave({
-        url: '/r/batches/',
+        url: '/core/api/parent',
         data: fields,
-        afterSave: () => navigate('/dashboard/batch'),
+        afterSave: () => navigate('/dashboard/parent'),
       });
     }
   };
   id = parseInt(id);
+
   useEffect(() => {
-    getCourses();
     if (id) {
-      const found = data.find(item => item.id === id);
+      const found = data.find(item => item.info === id);
       setItem(found);
+      setFirstName(found.first_name);
+      setLastName(found.last_name);
+      setBirthday(moment(found.birthday));
+      setIdPassport(found.id_number);
+      setPhoneNumber(found.phone_number);
+      setImgVal(process.env.REACT_APP_BACKEND_URL + found.avatar);
     }
   }, [item, data, id]);
 
+  const beforeUpload = file => {
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+      message.error('Image must smaller than 2MB!');
+    }
+
+    return false;
+  };
+
+  const handleChange = info => {
+    // Get this url from response in real world.
+
+    const reader = new FileReader();
+    reader.addEventListener('load', e => {
+      if (e && e.target) setImgVal(e.target.result);
+    });
+    reader.readAsDataURL(info.file);
+
+    setAvatar(info.file);
+  };
+
   return (
     <Card title={id ? 'Update parent' : 'Create New Parent'}>
-      {/* <BaseForm
-        fields={[
-          {
-            type: 'TEXT',
-            label: 'License plate',
-            name: 'name',
-            rules: [
-              {
-                required: true,
-                message: 'Name is required',
-              },
-            ],
-            defaultValue: item ? item.name : '',
-          },
-          {
-            type: 'TEXT',
-            label: 'Code',
-            name: 'code',
-            rules: [
-              {
-                required: true,
-                message: 'Code is required',
-              },
-            ],
-            defaultValue: item ? item.code : '',
-          },
-          {
-            type: 'SELECT',
-            label: 'Driver',
-            name: 'course_id',
-            options: courses.map(course => ({
-              value: course.id,
-              title: course.name,
-            })),
-            defaultValue: item
-              ? item.course_id
-              : courses.length > 0 && courses[0].id,
-          },
-
-          {
-            type: 'SELECT',
-            label: 'Quản sinh',
-            name: 'course_id',
-            options: courses.map(course => ({
-              value: course.id,
-              title: course.name,
-            })),
-            defaultValue: item
-              ? item.course_id
-              : courses.length > 0 && courses[0].id,
-          },
-        ]}
-        handleSubmit={handleSubmit}
-      /> */}
       <Form style={{ padding: 16 }} layout="horizontal">
         <Row gutter={16}>
           <Col offset={3} md={10}>
             <Row gutter={16}>
               <Col md={12}>
                 <Item label="First name" style={{ marginBottom: 12 }}>
-                  <Input />
+                  <Input
+                    onChange={e => setFirstName(e.target.value)}
+                    value={firstName}
+                  />
                 </Item>
               </Col>
               <Col md={12}>
                 <Item label="Last name">
-                  <Input />
+                  <Input
+                    onChange={e => setLastName(e.target.value)}
+                    value={lastName}
+                  />
                 </Item>
               </Col>
             </Row>
             <Row gutter={16}>
               <Col md={12}>
                 <Item label="Birthday">
-                  <DatePicker onChange={console.log} />
+                  <DatePicker
+                    value={birthday}
+                    onChange={val => setBirthday(val)}
+                  />
                 </Item>
               </Col>
               <Col md={12}>
-                <Item label="Phone">
-                  <Input />
+                <Item label="Phone number">
+                  <Input
+                    value={phoneNumber}
+                    onChange={e => setPhoneNumber(e.target.value)}
+                  />
                 </Item>
               </Col>
             </Row>
-            <Divider orientation="left">Address</Divider>
+            <Row gutter={16}>
+              <Col md={12}>
+                <Item label="ID/Passport number">
+                  <Input
+                    value={idPassport}
+                    onChange={e => setIdPassport(e.target.value)}
+                  />
+                </Item>
+              </Col>
+            </Row>
+            {!item && (
+              <div>
+                <Divider orientation="left" style={{ marginBottom: 0 }}>
+                  Account information
+                </Divider>
+                <Row gutter={16}>
+                  <Col md={12}>
+                    <Item label="Username">
+                      <Input onChange={e => setUsername(e.target.value)} />
+                    </Item>
+                  </Col>
+                  <Col md={12}>
+                    <Item label="Password">
+                      <Input
+                        type="password"
+                        onChange={e => setPassword(e.target.value)}
+                      />
+                    </Item>
+                  </Col>
+                </Row>
+              </div>
+            )}
+            <Divider orientation="left" style={{ marginBottom: 0 }}>
+              Address
+            </Divider>
             <Row gutter={16}>
               <Col md={12}>
                 <Item label="Home number">
-                  <Input />
+                  <Input
+                    value={homeNumber}
+                    onChange={e => setHomeNumber(e.target.value)}
+                  />
                 </Item>
               </Col>
               <Col md={12}>
                 <Item label="Ward">
-                  <Input />
+                  <Input
+                    value={homeWard}
+                    onChange={e => setHomeWard(e.target.value)}
+                  />
                 </Item>
               </Col>
             </Row>
             <Row gutter={16}>
               <Col md={12}>
                 <Item label="District">
-                  <Input />
+                  <Input
+                    value={homeDistrict}
+                    onChange={e => setHomeDistrict(e.target.value)}
+                  />
                 </Item>
               </Col>
               <Col md={12}>
                 <Item label="Province">
-                  <Input />
+                  <Input
+                    value={homeProvince}
+                    onChange={e => setHomeProvince(e.target.value)}
+                  />
                 </Item>
               </Col>
             </Row>
@@ -176,8 +237,8 @@ const ParentForm = ({ formSave, updateItem, id, data }) => {
             <Table
               columns={[
                 { title: 'Name', key: 'name' },
-                { title: 'Class', key: 'name' },
-                { title: 'Bus registered date', key: 'name' },
+                { title: 'Class', key: 'class' },
+                { title: 'Bus registered date', key: 'busregistereddate' },
               ]}
             />
           </Col>
@@ -187,17 +248,35 @@ const ParentForm = ({ formSave, updateItem, id, data }) => {
               listType="picture-card"
               className="avatar-uploader"
               showUploadList={false}
-              action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-              // beforeUpload={beforeUpload}
-              // onChange={this.handleChange}
+              //action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+              beforeUpload={beforeUpload}
+              onChange={handleChange}
             >
-              <div>
-                <Icon type={'plus'} />
-                <div className="ant-upload-text">Avatar</div>
-              </div>
+              {imgVal ? (
+                <img src={imgVal} alt="avatar" style={{ width: '100%' }} />
+              ) : (
+                <div>
+                  <Icon type={'plus'} />
+                  <div className="ant-upload-text">Avatar</div>
+                </div>
+              )}
             </Upload>
           </Col>
         </Row>
+        <Col
+          span={24}
+          style={{ display: 'flex', justifyContent: 'center', marginTop: 24 }}
+        >
+          <Button
+            type="primary"
+            htmlType="button"
+            onClick={handleSubmit}
+            style={{ marginRight: 24 }}
+          >
+            Save
+          </Button>
+          <Button onClick={() => window.history.back()}>Cancel</Button>
+        </Col>
       </Form>
     </Card>
   );
--- src/views/Dashboard/Parent/index.js
+++ src/views/Dashboard/Parent/index.js
@@ -1,51 +1,90 @@
 import React, { useState } from 'react';
-import { Card, Button, Popconfirm, Icon, Row } from 'antd';
+import { Card, Button, Popconfirm, Icon, Row, Tag } from 'antd';
 import { navigate } from '@reach/router';
 import { connect } from 'react-redux';
 import DataTable from 'components/DataTable';
-import GridView from 'components/GridView';
 import { actionCreator } from 'store/dataTable/dataTable.meta';
 
 export const Parent = props => {
   const [viewType, setViewType] = useState('LIST');
   const columns = [
     {
-      title: 'No',
-      dataIndex: 'no',
+      title: 'Avatar',
+      // dataIndex: 'avatar',
+      render: (_, record) => (
+        <img
+          style={{ width: '80px', height: '80px', textAlign: 'center' }}
+          src={process.env.REACT_APP_BACKEND_URL + record.avatar}
+        />
+      ),
     },
     {
-      title: 'Picture',
-      dataIndex: 'picture',
+      title: 'First name',
+      dataIndex: 'first_name',
     },
     {
-      title: 'First Name',
-      dataIndex: 'firstname',
+      title: 'Last name',
+      dataIndex: 'last_name',
     },
     {
-      title: 'Last Name',
-      dataIndex: 'lastname',
-    },
-    {
-      title: 'Phone Number',
-      dataIndex: 'phonenumber',
+      title: 'Phone number',
+      dataIndex: 'phone_number',
     },
     {
       title: 'Username',
       dataIndex: 'username',
     },
     {
-      title: 'Chilren',
-      dataIndex: 'chilren',
+      title: 'Children',
+      dataIndex: 'children',
     },
     {
       title: 'Status',
-      dataIndex: 'status',
+      render: (_, record) =>
+        record.status === 'A' ? (
+          <Tag color="#3e8247">Active</Tag>
+        ) : (
+          <Tag>Inactive</Tag>
+        ),
+      align: 'center',
+    },
+    {
+      title: 'Action',
+      align: 'center',
+      render: (_, record) => {
+        return (
+          <Row style={{ display: 'flex', justifyContent: 'center' }}>
+            <Button
+              style={{ marginRight: 16 }}
+              onClick={() => navigate(`/dashboard/parent/${record.id}`)}
+            >
+              <Icon type="form" />
+            </Button>
+            <Popconfirm
+              placement="top"
+              title={'Delete row?'}
+              onConfirm={() =>
+                props.deleteItem({
+                  url: `/core/api/parent/${record.id}`,
+                  afterDelete: () => props.getList({ url: '/core/api/parent' }),
+                })
+              }
+              okText="Yes"
+              cancelText="No"
+            >
+              <Button type="danger">
+                <Icon type="delete" />
+              </Button>
+            </Popconfirm>
+          </Row>
+        );
+      },
     },
   ];
 
   return (
     <Card
-      title="Manage Parents"
+      title="Manage parent"
       style={{ width: '100%', background: 'none' }}
       headStyle={{ backgroundColor: 'white' }}
       bodyStyle={{
@@ -58,8 +97,7 @@ export const Parent = props => {
         </Button>,
       ]}
     >
-      {viewType === 'LIST' && <DataTable columns={columns} />}
-      {viewType === 'CARD' && <GridView />}
+      <DataTable columns={columns} url="/core/api/parent" />
     </Card>
   );
 };
